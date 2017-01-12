f804608d681f0d50830402811b86ebb99c2340b2
JamesBollinger <jamesjamesjames198@gmail.com>
2017-01-11 21:48:48 -0500

    Update Archer.java

--- Archer.java
+++ Archer.java
@@ -2,20 +2,26 @@ public class Archer extends Character {
 	//used to determine maximum range
 	int range;
 	//is the type of bow, uses different arrow values for quantity and Armour piercing
-	Bow bow;
 	public Archer(int x, int y, int t){
 		super(x,y,"Archer.png",10,5,6,0,0,10,(int)(Math.random()*10), t);
 		range = 10;
-		bow = new Bow(10,5,3,new Arrow(2,3));
+		main = new Bow(10,5,3,new Arrow(2,3));
+		secondary = null;
+		if(main.ranged || secondary.ranged){
+			rangedWeapon = true;	
+		}
+		if(main.melee || secondary.melee){
+			meleeWeapon = true;	
+		}
 	}
 	//Used primarly for inconsole testing
 	public String toString(){
 		return "A";
 	}
 	//Is the attack function for the archer
-	public void attack(Character target){
+	public void attack(Character target, Weapon weapon){
 		if(!checkAdjacent(target.x,target.y) && target.team != this.team){
-			bow.attack(this, target);
+			weapon.attack(this, target);
 		}
 	}
 }
