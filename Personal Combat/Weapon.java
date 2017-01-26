8257d9693fd20e97faf8d89e9ea177c9cc2a205f
JamesBollinger <jamesjamesjames198@gmail.com>
2017-01-25 21:56:13 -0500

    Create Weapon.java

---
+++ Personal Combat/Weapon.java
@@ -0,0 +1,21 @@
+/*
+ * is the main weapon class
+ * all weapons are sub classes of this class
+ * is where the target actually gets assigned damage to take
+ */
+public abstract class Weapon{
+ int damage, armourPiercing;
+ boolean ranged, melee
+ public Weapon(int d, int aR){
+ 	damage = d; armourPiercing = aR;
+ }
+ public abstract void attack(Character attacker, Character target);
+ //This takes into account the targets armor, the armor piercing of the attack and the damage of the weapon
+ //if the armor piercing is greater than the armor, it is set to the difference is automatically set to zero
+ //it then deals a random amount of damage between the min and max damage range, with the armor difference subtracted from that
+ public void dealDamage(Character target){
+		int amourDifference = armourPiercing - target.armour;
+		if(amourDifference >= 0) amourDifference = 0;
+		target.dealDamage(damage - amourDifference));
+	}
+}
