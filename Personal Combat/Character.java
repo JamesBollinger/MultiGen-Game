3c470f933fb8ea9f50575610f89d60de82836fe4
JamesBollinger <jamesjamesjames198@gmail.com>
2017-01-25 21:54:53 -0500

    Update Character.java

--- Personal Combat/Character.java
+++ Personal Combat/Character.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /*
  * This is the main class that every entity currently is descended from
  * It contains the majority of methods that generally everyone would have to do(take damage, move, and not be down)
@@ -11,12 +12,14 @@ public abstract class Character extends Entity {
 	int health, strength, speed, dodge, armour, accuracy, initiative;
 	int team; //0 for friendly, 1 for enemy, -1 for no team
 	boolean up = true;
+	boolean meleeWeapon, rangedWeapon = false;
+	Weapon main;
+	Weapon secondary;
+	ArrayList<String> statuses = new ArrayList<String>();
 	public Character(int x,int y,String img, int h, int s, int sp, int d, int ar,int ac, int i, int t){
 		super(x,y,img);
 		health = h; strength = s; speed = sp; dodge = d; armour = ar;accuracy = ac; initiative = i; team = t;
 	}
-	public void move(int x,int y){this.x = x;this.y = y;}
-	public abstract void attack(Character target);
 	public void dealDamage(int d){
 		System.out.println(d);
 		health -= d;
@@ -35,9 +38,6 @@ public abstract class Character extends Entity {
 	public int getInit(){
 		return initiative;
 	}
-	public int getSpeed(){
-		return speed;
-	}
 	//All of these values should be inputed as either a plus of minus to change it up or down
 	public void setStrength(int change){
 		strength += change;	
@@ -55,7 +55,44 @@ public abstract class Character extends Entity {
 		accuracy += change;	
 	}
 	public void setIntiative(int change){
-		initiative += change;	
+		intiative += change;	
 	}
-
+	
+	//Basic Combat Interactions
+	public void move(int x,int y){this.x = x;this.y = y;}
+	//The attack methods have been combined, with which weapons and whether it is a ranged attack being down at a lower level
+	//Actual computation of dodging and accuracy will be done at lower levels
+	//will also assume it is a valid attack;
+	public void attack(Character target, Weapon weapon){
+		weapon.attack(this,target);
+	}
+	public void alter(Character target, String stat, int change){
+		if(stat.equals("dodge")) target.setDodge(change);
+		else if(stat.equals("strength")) target.setStrength(change);
+		else if(stat.equals("armour")) target.setArmour(change);
+		else if(stat.equals("accuracy")) target.setAccuracy(change);
+		else if(stat.equals("intiative")) target.setIntiative(change);
+		else if(stat.equals("speed")) target.setSpeed(change);
+		else System.out.println("invalid attribute");
+	}
+	//For the statuses it will simply add the status string to a string arrayList and then at lower levels in conjunction with action listener, block actions or add modifiers
+	public void status(Character target, String status){
+		if(!checkExists(statuses,status))target.statuses.add(status);	
+	}
+	//I may think about creating a status class and creating a HashMap for this, but that would come later and not be hard to quickly implement
+	public boolean checkExists(ArrayList<String> in, String input){
+		for(int i = 0; i < in.size(); i++){
+			if(input.equals(in.get(i)) return true;
+		}
+		return false;
+	}
+	public String removeElem(ArrayList<String> in, String input){
+		for(int i = 0; i < in.size(); i++){
+			if(input.equals(in.get(i)){
+				in.remove(i);
+				return input;
+			}
+		}
+		return null;
+	}
 }
