e00561ddd5cc988a3c9ea52ce7bb6eb32ca5d768
JamesBollinger <jamesjamesjames198@gmail.com>
2017-01-28 14:09:25 -0500

    Update Weapon.java

--- Personal Combat/Weapon.java
+++ Personal Combat/Weapon.java
@@ -5,6 +5,7 @@
  */
 public abstract class Weapon{
  int damage, armourPiercing;
+ int k = 2; //This is the value that we will be shifting for balance purposes
  boolean ranged, melee
  public Weapon(int d, int aR){
  	damage = d; armourPiercing = aR;
@@ -13,9 +14,13 @@ public abstract class Weapon{
  //This takes into account the targets armor, the armor piercing of the attack and the damage of the weapon
  //if the armor piercing is greater than the armor, it is set to the difference is automatically set to zero
  //it then deals a random amount of damage between the min and max damage range, with the armor difference subtracted from that
- public void dealDamage(Character target){
-		int amourDifference = armourPiercing - target.armour;
-		if(amourDifference >= 0) amourDifference = 0;
-		target.dealDamage(damage - amourDifference));
+ public void dealDamage(Character attacker, Character target, int modifiers){
+		int armorAbsorption;
+	 	if(armourPiercing >= target.armor) armorAbsorption = 0;
+	 	else armorAbsorption = (target.armor - armourPiercing)*k;
+	 	int attackPower = damage + attacker.strength + modifiers;
+	 	if(attackPower - armorAbsorption <= 0){
+			target.dealDamage(attackPower - armorAbsorption);	
+		}
 	}
 }
